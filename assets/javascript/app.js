9623b950291cd76c35a84115c2d8579812ff37fa
Brenton Lake <Brenton Lake>
2016-11-23 15:55:01 -0600

    update js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
 
-	var question1;
-	var question2;
-	var question3;
-	var question4;
-	var question5;
-	var question6;
-	var question7;
-	var question8;
-	var question9;
-	var question10;
-	var correct;
-	var incorrect;
-	var timesup;
-	var noanswer;
+	var questions;
+	
+	var timeUp;
+	
 
 	var questionTime ={
 		time: 30,
@@ -24,7 +14,7 @@ $(document).ready(function() {
         },
 		start: function(){
 			$("timer").html("Time Left: " + questionTime.time);
-			counter = setInterval(questionTime.count, 1000);
+			var counter = setInterval(questionTime.count, 1000);
 		},
 		stop: function(){
         	clearInterval(counter);
@@ -36,85 +26,155 @@ $(document).ready(function() {
 
     	}
 
-	}
+	}	
+
 		function varSet(){
 
-		question1 = {
+		questions = [{
 			question: "Who sang 'The Battle Evermore'?",
 			answers: ["Queen", "The Beatles", "Led Zeppelin", "Rush"],
 			correctanswer: 1
-			gifright: 'assets/images/ledzeppelin.gif',
-			gifwrong: 'assets/images/ledzeppelin.gif',
-		}
-		question2 = {
+		
+		},
+		 {
 			question: "Who sang the song 'Barracuda'?",
 			answers: ["Heart", "Genesis", "Blondie", "ZZ Top"],
 			correctanswer: 0
-			gifright: 'assets/images/heart1.gif',
-			gifwrong: 'assets/images/heart1.gif',
-		}	
-		question3 = {
+			
+		},	
+		 {
 			question: "Who sang 'Miss America'?",
 			answers: ["REO Speed Wagon", "The Rolling Stones", "The Beatles", "Styx"],
 			correctanswer: 3
-			gifright: 'assets/images/styx.gif',
-			gifwrong: 'assets/images/styx.gif',
-
-		}
-		question4 = {
+			
+		},
+		{
 			question: "'I Am The Walrus' was sung by?",
 			answers: ["The Rolling Stones", "The Monkees", "The Beatles", "The Silver Beatles"],
 			correctanswer: 2
-			gifright: 'assets/images/beatles1.gif',
-			gifwrong: 'assets/images/beatles1.gif',
-		}
-		question5 = {
+			
+		},
+		{
 			question: "What group had the hit 'Magic Men'?",
 			answers: ["Blondie", "Heart", "Queen", "Bread"],
 			correctanswer: 2
-			gifright: 'assets/images/heart.gif',
-			gifwrong: 'assets/images/heart.gif',
-		}
-		question6 = {
+		
+		},
+		{
 			question: "'Runnin' with the Devil' was a hit for?",
 			answers: ["Van Halen", "Ritchie Valens", "Rush", "Styx"],
 			correctanswer: 0
-			gifright: 'assets/images/vanhalen.gif',
-			gifwrong: 'assets/images/vanhalen.gif',
-		}
-		question7 = {
+			
+		},
+		{
 			question: "Who had a hit called'The Immigrant Song'?",
 			answers: ["The Doors", "The Byrds", "Led Zeppelin", "Queen"],
 			correctanswer: 2
-			gifright: 'assets/images/zeppelin2.gif',
-			gifwrong: 'assets/images/zeppelin2.gif',
-		}
-		question8 = {
+			
+		},
+		 {
 			question: "Who sang 'Do You Want To Know A Secret?",
 			answers: ["The Rolling Stones", "The Beatles", "John Lennon", "Mick Jagger"],
 			correctanswer: 1
-			gifright: 'assets/images/beatles2.gif',
-			gifwrong: 'assets/images/beatles2.gif',
-		}
-		question9 = {
+			
+		},
+		{
 			question: "'Walk This Way' was sung by?",
 			answers: ["Styx", "Cream", "Boston", "Aerosmith"],
 			correctanswer: 3
-			gifright: 'assets/images/aerosmith.gif',
-			gifwrong: 'assets/images/aerosmith.gif',
-		}
-		question10 = {
+		
+		},
+		{
 			question: "'Rock of Ages' was sung by?",
 			answers: ["Led Zeppelin", "Def Leopard", "ZZ Top", "Boston"],
 			correctanswer: 1
-			gifright: 'assets/images/defleopard.gif',
-			gifwrong: 'assets/images/defleopard.gif',,
-		}
+			
+		}]
 	}
+		
+			});
+	var correct = 0;
+	var incorrect = 0;
+	var noanswer = 0;
+	var Index = -1;
+
+	
+
+	
+		$('#questions').html("<button class='btn' id='start'>Start</button>")
+		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
+
+		$('#start').on("click", function() {
+			nextQuestion();
+		});
+	
+
+
+	function askQuestions(){
+
+
+		questionTime.start();
+		$('#questions').html(questions.question);
+		$('#answer0').show().html(questions[Index].answers[0]);
+		$('#answer1').show().html(questions[Index].answers[1]);
+		$('#answer2').show().html(questions[Index].answers[2]);
+		$('#answer3').show().html(questions[Index].answers[3]);
+
+		onClickAnswer();
+	}
+	function onClickAnswer() {
+		$('.btn').on("click", function() {
+			var btnClick = parseInt($(this).attr('value'));
+			if(btnClick === qustions[Index].correctanswer) {
+				rightAnswer();
+			}
+			else {
+				wrongAnswer();
+			}
+		});
+	}
+
+	function wrongAnswer() {
+		clearTimeout(timeIsUp);
+		wrong++;
+		questionTimer.stop();
+		questionTimer.reset();
+		$('#time').empty();
+		$('#question').text("Wrong Answer");
+		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
+		
+
+		timeUp = setTimeout(advance, 4 * 1000);
+	
+	function rightAnswer() {
+		clearTimeout(timeUp);
+		right++;
+		questionTime.stop();
+		questionTime.reset();
+		$('#time').empty();
+		$('#questions').text("Correct!");
+		$('#answer0, #answer1, #answer2, #answer3').hide().off('click');
+
+
+		timeUp = setTimeout(advance, 4 * 1000);
+	}
+}
+	function nextQuestion() {
+		var Index = -1;
+		Index++;
+
+		if(Index < questions.length) {
+			askQuestions();
+		varSet();
+		
+	}
+}
+
 		
 
 
 
 
 
-}
+
+
